f7786c098ca24267a68ea88d767cffe55a5a1dfd
ixjsa12 <155607549+ixjsa12@users.noreply.github.com>
2024-01-04 13:23:32 +0800

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -17,7 +17,7 @@ export default {
 
 	if (!actualUrlStr) {
 		const mainDomain = url.hostname;
-		const websiteTitle = "Anti proxy for Mingyu"; // 请替换为您的网站标题
+		const websiteTitle = "Xh SITE PROXY"; // 请替换为您的网站标题
 		const errorMessage = `
 		<html>
 		    <head>
@@ -128,7 +128,6 @@ export default {
 		          <input type="text" id="url" name="url" placeholder="例如：https://github.com/" />
 		          <input type="button" id="submit" value="进入代理" onclick="redirectToProxy()" />
 		        </div>
-		  <p>&copy; 2023 <a href="https://github.com/ymyuuu/" target="_blank">Mingyu</a></p>
 		      </div>
 		      <script>
 		        function redirectToProxy() {
